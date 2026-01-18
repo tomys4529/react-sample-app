b66a14aeff947fed9718def93e534842f18a5815
tomys4529 <glay45290615@gmail.com>
2026-01-18 12:41:30 +0900

    15-20の学習

--- react-sample-app/020_arrow_function/main.js
+++ react-sample-app/020_arrow_function/main.js
@@ -130,4 +130,91 @@ const calc = (a, b) => {
 とくにReactは関数だらけの処理になるのでアロー関数を使う利点になる
 配列処理は全部アロー関数で書くのが基本でコールバック関数との相性も抜群にいい
 
+★★DOMについて★★
+htmlのタグをjsから操作するときに使用するもの
+htmlではタグは使用できないのでDOMを通してオブジェクトとして操作できるように使用する事が出来る
+
+例
+<h1>title</h1>
+
+const h1Element = document.querySelector('h1');
+console.dir(h1Element); ※titleとコンソールに表示
+console.log(h1Element.textContent); ※changed titleと表示
+h1Element.textContent = 'changed title'
+
+上記のコードだと元に記述していたh1タグのタイトルを、「changed title」に変更する事が出来る
+
+・イベントリスナーについて
+何かをトリガーとして処理が発火する事が出来る動き
+
+例
+<button>click</button>
+
+const btnEl = querySelector('button'); ※クエリセレクタとしてbuttonタグを指定
+btnEl.addEventListener('click', () => { ※イベントリスナーにクリックしたときにhelloとコンソール表示するように処理を書いている
+  console.log('hello')
+})
+
+addEventListenerでイベントリスナーとして中に処理を書く（今回はクリックイベントを使用してクリックしたときの動きを書いている）
+
+イベントに伴う情報を取得したい場合は、下記の記述で取得可能
+今回は、eという引数からtargetというプロパティを通して情報を取得している
+const btnEl = querySelector('button');
+btnEl.addEventListener('click', (e) => {
+  console.dir(e.target);
+  console.log('hello')
+})
+
+・配列のmap,filterについては理解しているので割愛
+
+・分割代入について
+配列を変数に代入し取得する事が出来る
+
+例
+const [a , , c ] = ["obj1", "obj2", "obj3"];
+console.log(a); ※obj1が取得される
+console.log(c); ※obj3が取得される
+★この場合、取得するプロパティの順番が重要になる。
+
+const {z , x } = {x: "obj1", y: "obj2", z: 'obj3};
+console.log(x); ※obj1が取得される
+console.log(z); ※obj3が取得される
+
+例2
+配列の分割代入
+const fnArr = ([country, state, city ]) => {
+  console.log("-配列-");
+  console.log(`country: ${country}`);
+  console.log(`state: ${state}`);
+  console.log(`city: ${city});
+};
+
+変数の分割代入
+const fnObj = ({ country, state }) => {
+  console.log("-obj-");
+  console.log(`country: ${country}`);
+  console.log(`state: ${state}`);
+  cityのプロパティを使わない場合、書かなくてもよい。
+
+・スプレッド演算子について
+
+例
+let arr1 = [1, 2, 3];
+let arr2 = [4, 5, 6];
+let newArr = [...arr1];
+let newArr1 = arr1;
+newArr.push(4)
+console.log(newArr);
+この場合、スプレッド演算子で取得されるため、pushで4を代入されていたとしても、
+元のarr1の「1,2,3」しか取得されない。
+newArrはarr1と全く別の新しい配列として扱われるため。
+
+console.log(arr1)
+この場合、arr1に定義されている値を丸ごと取ってくるので、pushで4が代入されていてもコンソールには「1,2,3,4」が取得される
+
+let newArr = [...arr1, ...arr2];
+と宣言する事により、
+console.log(newArr);
+で結果を取得すると、[1, 2, 3, 4, 5, 6]と結合させて取得する事もできる
+};
 */
